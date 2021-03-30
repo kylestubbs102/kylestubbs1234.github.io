6ad6319cddbd1f98a97ec920f4cf0103c7fff6a5
kylestubbs1234 <kylestubbs@ucsb.edu>
2021-03-30 13:29:05 -0700

    set up navbar with react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import Navbar from './components/Navbar.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Home } from './components/Pages/Home';
+import { About } from './components/Pages/About';
+import { Experience } from './components/Pages/Experience';
+import { Contact } from './components/Pages/Contact';
+import { Projects } from './components/Pages/Projects';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/experience" component={Experience} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
