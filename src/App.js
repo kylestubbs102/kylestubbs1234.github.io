3478ac1b31407030b1e0334c159b633932a6661d
kylestubbs1234 <kylestubbs@ucsb.edu>
2021-10-05 00:16:07 -0700

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar.js';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import { Home } from './components/Pages/Home';
 import { About } from './components/Pages/About';
 import { Experience } from './components/Pages/Experience';
